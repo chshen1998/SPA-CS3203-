4dab39c54cedca1efa5e20477336db9ad8f195ce
chshen1998 <chewhoashen@gmail.com>
2022-09-07 19:41:53 +0800

    Uncomment code

--- Team37/Code37/src/spa/src/SP/SP.cpp
+++ Team37/Code37/src/spa/src/SP/SP.cpp
@@ -6,9 +6,9 @@ using namespace std;
 #include "AST/SourceCode.h"
 #include "SP.h"
 
-//shared_ptr<TNode> SP::parse(string filename) {
-//    shared_ptr<ParserVisitor> parserVisitor = make_shared<ParserVisitor>();
+shared_ptr<TNode> SP::parse(string filename) {
+    shared_ptr<ParserVisitor> parserVisitor = make_shared<ParserVisitor>();
 
-//    SourceCode sourceCode = SourceCode(filename);
+    SourceCode sourceCode = SourceCode(filename);
 //    sourceCode.accept(parserVisitor);
-//}
+}

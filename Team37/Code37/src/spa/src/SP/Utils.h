b54e3ab2c344b13dfaa2a5040f3c9c371a1eee4b
sueann-chua <sue.ann.chua@gmail.com>
2022-09-11 22:22:50 +0800

    Method to find all matching sets of open and closed egyptian brackets in a procedure string

--- Team37/Code37/src/spa/src/SP/Utils.h
+++ Team37/Code37/src/spa/src/SP/Utils.h
@@ -4,6 +4,7 @@ using namespace std;
 #define SP_UTILS_H
 
 #include <string>
+#include <vector>
 
 class Utils {
 public:
@@ -29,5 +30,32 @@ public:
      * @return trimmed string
      */
     static string trim(string s);
+
+    /**
+     * Recursively gets indexes of all open egyptian brackets in procedure string.
+     * @param procedure procedure as a string
+     * @param openIndexes vector to store indexes of open egyptian brackets
+     * @param offset offset from the start of the string
+     * @return Indexes of all open egyptian brackets in procedure.
+     */
+    static vector<int> getOpenIndexes(string procedure, vector<int> openIndexes, int offset);
+
+    /**
+     * Recursively gets indexes of all close egyptian brackets in procedure string.
+     * @param procedure procedure as a string
+     * @param openIndexes vector to store indexes of open egyptian brackets
+     * @param offset offset from the start of the string
+     * @return Indexes of all close egyptian brackets in procedure.
+     */
+    static vector<int> getClosedIndexes(string procedure, vector<int> closedIndexes, int offset);
+
+    /**
+     * Matches the open and close egyptian brackets to each other by finding the nearest close egyptian
+     * bracket for each open bracket.
+     * @param openIdx Indexes of all open egyptian brackets in procedure
+     * @param closedIdx Indexes of all close egyptian brackets in procedure
+     * @return vector containing all pairs of indexes (index of open bracket, index of close bracket)
+     */
+    static vector<vector<int> > getSets(vector<int> openIdx, vector<int> closedIdx);
 };
 #endif //SP_UTILS_H

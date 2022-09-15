5104b7131cce3072e5b64eb70175f27d2dfbc150
kieron560 <kieron560@gmail.com>
2022-09-15 22:48:49 +0800

    Sprint 1 code

--- Team37/Code37/src/spa/src/QPS/QueryEvaluator.cpp
+++ Team37/Code37/src/spa/src/QPS/QueryEvaluator.cpp
@@ -19,34 +19,14 @@ QueryEvaluator::QueryEvaluator(PqlQuery pqlQuery, shared_ptr<QueryServicer> s, l
 }
 
 void QueryEvaluator::evaluate() {
-    QuerySelect();
-}
-
-void QueryEvaluator::QuerySelect() {
     const string selectSynonym = pq.select;
     const TokenType type = pq.declarations[selectSynonym];
-    set<string> setOfStrings;
-
+    
     if (type == TokenType::VARIABLE) {
         set<NameExpression> vars = servicer->getAllVar();
-        
+
         for (auto v : vars) {
             result.push_back(v.getVarName());
         }
     }
-    
-    else if (type == TokenType::CONSTANT) {
-        set<ConstantExpression> consts = servicer->getAllConst();
-
-        for (auto c : consts) {
-            setOfStrings.insert(to_string(c.getValue()));
-        }
-    }
-
-    else {}
-   
 }
-
-
-
-

ff636c9bcfaaa6e6898263859492ce37e04eb57e
kieron560 <kieron560@gmail.com>
2022-09-15 23:41:49 +0800

    Add tests

--- Team37/Code37/src/autotester/src/TestWrapper.cpp
+++ Team37/Code37/src/autotester/src/TestWrapper.cpp
@@ -27,10 +27,10 @@ TestWrapper::~TestWrapper() {
 void TestWrapper::parse(string filename) {
 	shared_ptr<SourceCode> AST = SP::parse(filename);
 	knowledgeBase->buildFromAst(AST);
-	queryProcessor->setQueryServicer(knowledgeBase->getQueryServicer());
+	//queryProcessor->setQueryServicer(knowledgeBase->getQueryServicer());
 }
 
 // method to evaluating a query
 void TestWrapper::evaluate(string query, list<string>& results){
-	 queryProcessor->evaluate(query, results);
+	 //queryProcessor->evaluate(query, results);
 }

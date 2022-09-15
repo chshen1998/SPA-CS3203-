3e50a55dc82b94c69039bf563620a1584ed1d18b
soaza <chukimguan@gmail.com>
2022-09-15 15:39:18 +0800

    remove unused fields

--- Team37/Code37/src/spa/src/AST/ASTVisitor/ExtractGeneralASTVisitor.h
+++ Team37/Code37/src/spa/src/AST/ASTVisitor/ExtractGeneralASTVisitor.h
@@ -11,8 +11,6 @@ using namespace std;
 
 class ExtractGeneralASTVisitor : public ASTVisitor, public enable_shared_from_this<ASTVisitor> {
 private:
-    vector<shared_ptr<NameExpression>> VisitedTNodeVariables;
-    vector<shared_ptr<ConstantExpression>> VisitedConstants;
     shared_ptr<Storage> storage;
 public:
     // Constructor

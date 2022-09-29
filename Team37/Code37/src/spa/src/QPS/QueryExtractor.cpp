3d3f8f0c36e4505ef8fc91624d30ac203bcecffc
chshen1998 <chewhoashen@gmail.com>
2022-09-30 01:00:35 +0800

    Extract With Clause

--- Team37/Code37/src/spa/src/QPS/QueryExtractor.cpp
+++ Team37/Code37/src/spa/src/QPS/QueryExtractor.cpp
@@ -11,7 +11,8 @@ using namespace std;
 #include "./Types/ErrorType.h"
 #include "./Types/TokenType.h"
 
-QueryExtractor::QueryExtractor(vector<PqlToken> tokenVector) {
+QueryExtractor::QueryExtractor(vector<PqlToken> tokenVector)
+{
     tokens = tokenVector;
     size = tokens.size();
     next = 0;
@@ -19,14 +20,16 @@ QueryExtractor::QueryExtractor(vector<PqlToken> tokenVector) {
  
 }
 
-PqlQuery QueryExtractor::extractSemantics() {
+PqlQuery QueryExtractor::extractSemantics()
+{
     extractDeclarations();
     extractSelect();
     extractClauses();
     return pq;
 }
 
-void QueryExtractor::extractDeclarations() {
+void QueryExtractor::extractDeclarations()
+{
     PqlToken declaration = getNextToken();
     while (declaration.type != TokenType::DECLARATION_END) {
         PqlToken synonym = getNextToken();
@@ -39,13 +42,15 @@ void QueryExtractor::extractDeclarations() {
     }
 }
 
-void QueryExtractor::extractSelect() {
+void QueryExtractor::extractSelect()
+{
     getNextToken();
     const PqlToken synonym = getNextToken();
     pq.select = synonym.value;
 }
 
-void QueryExtractor::extractClauses() {
+void QueryExtractor::extractClauses()
+{
     PqlToken nextToken = getNextToken();
 
     while (nextToken.type != TokenType::END)
@@ -65,17 +70,22 @@ void QueryExtractor::extractClauses() {
     }
 }
 
-PqlToken QueryExtractor::extractPatternClause() {
+PqlToken QueryExtractor::extractPatternClause()
+{
+    PqlToken pattern;
+    PqlToken left;
+    PqlToken right;
+
     PqlToken next = PqlToken(TokenType::AND, "and");
     while (next.type == TokenType::AND)
     {
-        PqlToken synonym = getNextToken();
+        pattern = getNextToken();
         getNextToken(); // OPEN BRACKET
-        PqlToken left = getNextToken();
+        left = getNextToken();
         getNextToken(); // COMMA
-        PqlToken right = getNextToken();
+        right = getNextToken();
         getNextToken(); // CLOSE BRACKET
-        pq.clauses.push_back(PatternClause(synonym, left, right));
+        pq.clauses.push_back(PatternClause(pattern, left, right));
         next = getNextToken();
     }
     return next;
@@ -83,27 +93,58 @@ PqlToken QueryExtractor::extractPatternClause() {
 
 PqlToken QueryExtractor::extractWithClause()
 {
+    PqlToken left;
+    PqlToken leftAttr;
+    PqlToken right;
+    PqlToken rightAttr;
+
     PqlToken next = PqlToken(TokenType::AND, "and");
     while (next.type == TokenType::AND)
     {
-	    
+        left = getNextToken();
+        leftAttr = PqlToken();
+
+        next = getNextToken(); // Either "." or "="
+        if (next.type == TokenType::DOT)
+        {
+            leftAttr = getNextToken();
+            getNextToken(); // "="
+        }
+
+        right = getNextToken();
+        rightAttr = PqlToken();
+
+        next = getNextToken(); // Either "." or "="
+        if (next.type == TokenType::DOT)
+        {
+            rightAttr = getNextToken();
+        }
+
+        pq.clauses.push_back(WithClause(PqlToken(), left, right, leftAttr, rightAttr));
+
+        next = getNextToken(); // Either "and" or next clause type
     }
 
     return next;
 }
 
 
-PqlToken QueryExtractor::extractSuchThatClause() {
-    getNextToken(); // THAT
+PqlToken QueryExtractor::extractSuchThatClause()
+{
+	getNextToken(); // THAT
+
+    PqlToken suchThatClause;
+    PqlToken left;
+    PqlToken right;
 
     PqlToken next = PqlToken(TokenType::AND, "and");
     while (next.type == TokenType::AND) 
     {
-	    PqlToken suchThatClause = getNextToken();
+	    suchThatClause = getNextToken();
 	    getNextToken(); // OPEN BRACKET
-	    PqlToken left = getNextToken();
+	    left = getNextToken();
 	    getNextToken(); // COMMA
-	    PqlToken right = getNextToken();
+	    right = getNextToken();
 	    getNextToken(); // CLOSE BRACKET
 	    pq.clauses.push_back(SuchThatClause(suchThatClause, left, right));
         next = getNextToken();
@@ -112,7 +153,8 @@ PqlToken QueryExtractor::extractSuchThatClause() {
     return next;
 }
 
-PqlToken QueryExtractor::getNextToken() {
+PqlToken QueryExtractor::getNextToken()
+{
     if (next == size)
     {
         return PqlToken(TokenType::END, "");

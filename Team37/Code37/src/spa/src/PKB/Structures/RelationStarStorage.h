f3815c4993898a3e16c64010e6a710ad3b09cd35
soaza <chukimguan@gmail.com>
2022-10-05 14:09:20 +0800

    fix build error for mac

--- Team37/Code37/src/spa/src/PKB/Structures/RelationStarStorage.h
+++ Team37/Code37/src/spa/src/PKB/Structures/RelationStarStorage.h
@@ -7,52 +7,53 @@
 #include <unordered_map>
 #include <map>
 #include "RelationStorage.h"
+
 using namespace std;
 
-template <typename S, typename T> class RelationStarStorage : public RelationStorage<S,T> {
+template<typename S, typename T>
+class RelationStarStorage : public RelationStorage<S, T> {
 public:
     // Store
-	RelationStarStorage<S,T> buildStar() {
-		RelationStarStorage<S, T> output = RelationStarStorage<S, T>();
+    RelationStarStorage<S, T> buildStar() {
+        RelationStarStorage<S, T> output = RelationStarStorage<S, T>();
 
 
-		for (multimap<S, T>::iterator itr = forwardStore.begin(); itr != forwardStore.end(); ) {
-			S key = itr->first;
-			unordered_map<T, bool> memo;
-			unordered_set<T> workingSet;
+        for (typename multimap<S, T>::iterator itr = this->forwardStore.begin(); itr != this->forwardStore.end();) {
+            S key = itr->first;
+            unordered_map<T, bool> memo;
+            unordered_set<T> workingSet;
+            vector<T> initalRelation = this->forwardRetrieve(key);
 
-			vector<T> initalRelation = forwardRetrieve(key);
+            for (T x: initalRelation) {
+                workingSet.insert(x);
+            }
 
-			for (T x : initalRelation) {
-				workingSet.insert(x);
-			}
+            while (!workingSet.empty()) {
+                T currVal = *workingSet.begin();
+                workingSet.erase(workingSet.begin());
+                output.store(key, currVal);
 
-			while (!workingSet.empty()) {
-				T currVal = *workingSet.begin();
-				workingSet.erase(workingSet.begin());
-				output.store(key, currVal);
+                // Add all that forwardRetreive
+                vector<T> nextVals = this->forwardRetrieve(currVal);
 
-				// Add all that forwardRetreive
-				vector<T> nextVals = forwardRetrieve(currVal);
-
-				for (T x : nextVals) {
-					// Check if done before
-					if (memo[x]) {
-						continue;
-					}
-					workingSet.insert(x);
-					memo[x] = true;
-				}
-			}
+                for (T x: nextVals) {
+                    // Check if done before
+                    if (memo[x]) {
+                        continue;
+                    }
+                    workingSet.insert(x);
+                    memo[x] = true;
+                }
+            }
 
 
-			// Till the next key
-			multimap<S, T>::iterator curr = itr;
-			while (itr != forwardStore.end() && itr->first == curr->first)
-				++itr;
-		}
+            // Till the next key
+            typename multimap<S, T>::iterator curr = itr;
+            while (itr != this->forwardStore.end() && itr->first == curr->first)
+                ++itr;
+        }
 
-		return output;
+        return output;
     }
 
 };
